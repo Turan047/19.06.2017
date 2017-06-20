51cd32f14b7ad8ef19c86a12203a2884c8e8ae26
Turan047 <hasanli_2ran@hotmail.com>
2017-06-20 14:26:55 +0400

    Add files via upload
    Yenilenmish versiya

---
+++ Yenisi_Average/Average/Average/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            /*List<int> reqem = new List<int>();
+            reqem.Add(2);
+            reqem.Add(3);
+            reqem.Add(7);
+            reqem.Add(4);
+            reqem.Add(9);
+            int i = reqem[0];
+            */
+            Console.WriteLine("Enter you number: ");
+            int reqem = Convert.ToInt32(Console.ReadLine());
+
+            int cem = 0;
+            int j = 0;
+            while (reqem!=0)
+            {
+                int i = reqem % 10;              
+                cem = i + cem;
+                reqem = reqem / 10;
+                j++;
+            }
+            Console.WriteLine(cem/j);
+        }
+    }
+}
